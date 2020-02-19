88b86eb2b04a0c5d6609d880f3c8677e84b53ac8
slipihollow <32129616+slipihollow@users.noreply.github.com>
2020-02-19 20:18:19 +0100

    Add files via upload

---
+++ Godot_menu/New Game Button.gd
@@ -0,0 +1,14 @@
+extends Button
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup_centered(Vector2 size=Vector2( 0, 0 ) )
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Godot_menu/WindowDialog.gd
@@ -0,0 +1,12 @@
+extends WindowDialog
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup(10,10)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
