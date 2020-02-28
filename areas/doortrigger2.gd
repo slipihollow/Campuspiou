327038b96defd0e6c4adf24fda08bc73269ddc4f
amorenza <55991112+amorenza@users.noreply.github.com>
2020-02-28 22:40:17 +0100

    Add files via upload

--- areas/doortrigger2.gd
+++ areas/doortrigger2.gd
@@ -8,24 +8,17 @@ func _ready():
 func _on_doortrigger2_body_entered(body):
 	print(body)
 	if body.name == "player":
+		set_process(true)
+		player = body
 		var spawn_data =  {
 				"spawn_on_node": "house/door/doortrigger",
 				}
-		set_process(true)
-		player = body
-		SceneManager.goto_scene("res://areas/test1.tscn",spawn_data)
+		SceneManager.goto_scene("res://areas/ppal.tscn",spawn_data)
+			#get_tree().change_scene("res://areas/test1.tscn")
+
 
 func _on_doortrigger2_body_exited(body):
 	if body.name =="player":
 		set_process(false)
 		player = body
 		
-#func set_process(delta):
-	#if player:
-		#if player and $anim.play("walkdown"):
-			
-			#SceneManager.goto_scene("res://areas/test1.tscn",spawn_data)
-			#get_tree().change_scene("res://areas/test1.tscn")
-
-
-
